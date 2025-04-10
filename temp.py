1013e1c14f338017a9bcb36ca6fc234185441c8a
shadow <email2agastya@gmail.com>
2025-04-10 11:42:16 -0700

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+#Line Graph
+plt.title("Tmperature of the week")
+plt.plot([25,22,23,23,23,22,25],["Mon","Tue","Wed","Thur","Fri","Sat","Sun"],label="Line 1")
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.show()
