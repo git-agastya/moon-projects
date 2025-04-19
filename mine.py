61b0c79bb6bf98362ca6b3438da88c96752ad1ea
shadow <email2agastya@gmail.com>
2025-04-19 16:58:21 -0700

    Add files via upload

---
+++ mine.py
@@ -0,0 +1,11 @@
+"""
+How to run this ?
+streamlit run ./mine.py
+"""
+
+import streamlit as st
+st.title("Minecraft")
+st.header("About Minecraft which is founded by Markus Persson")
+st.write("Minecraft is a viral and fun game. You can be creative and go to different worlds. You can play online or offline and also with friends. Their are different" \
+"materials you can use to make different objects. Minecrft is not all a peaceful game, there will be monsters like zombies, skeletons, and a tall, slim, creature known as the" \
+"enderman, and more. You will have to defeat the monsters with weapons, like swords, and more. Do not forget to play!💀⚔️")
