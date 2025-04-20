77e8e8fa28914d1f4d8810d928328a3076ffb557
shadow <email2agastya@gmail.com>
2025-04-20 12:14:01 -0700

    Add files via upload

---
+++ anime.py
@@ -0,0 +1,16 @@
+"""
+How to run?
+streamlit run ./anime.py
+"""
+import streamlit as st
+st.title("My 5 fav. Anime Characters and their Powers")
+st.header("Kakashi Hatake")
+st.write("He is the sensei of Naruto and his powers are that he knows most of the jujitsus he has the sharigan eye(chakra) and he has lightning touch.")
+st.header("Sasuke Uchiha")
+st.write("Sasuke is one of Naruto's greatest enemies and his powers are ice release, sharingan, and lightning, and even more.")
+st.header("Inosuke Hashibira")
+st.write("Inosuken is an ally of Tanjiro and has the powers like strength fangs, swords, and also etreme flexibility.")
+st.header("Katakuri")
+st.write("Katakuri is like an enemy and ally to Monkey D. Luffy and has the Mochi-Michi fruit in Onepiece.")
+st.header("Naruto")
+st.write("Naruto is the main character of Naruto and has the powers like ninjas and his sensei, Kakashi Hatake.")
